4eebcc4566668d0d712f6d4b8072716ae60a6c95
강다솔 <dasol199@naver.com>
2024-01-17 12:42:15 +0900

    :sparkles: 프로젝트 생성

---
+++ backend/ieum/src/test/java/com/ukcorp/ieum/IeumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ukcorp.ieum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
