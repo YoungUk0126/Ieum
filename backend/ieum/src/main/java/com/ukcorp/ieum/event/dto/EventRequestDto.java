2da179b8e53430acd5bcef3cdb13bfba10f1e27c
강다솔 <dasol199@naver.com>
2024-01-17 14:33:50 +0900

    :sparkles: [init] EventDto 추가

---
+++ backend/ieum/src/main/java/com/ukcorp/ieum/event/dto/EventRequestDto.java
@@ -0,0 +1,17 @@
+package com.ukcorp.ieum.event.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class EventRequestDto {
+    private String eventName;
+    private LocalDate eventDate;
+}
