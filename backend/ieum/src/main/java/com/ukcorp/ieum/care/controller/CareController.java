f589b6d4da5143c228ffbf11faf0ddbaa5473cfc
강다솔 <dasol199@naver.com>
2024-01-17 14:21:34 +0900

    :sparkles: Feat: CareController 추가

--- backend/ieum/src/main/java/com/ukcorp/ieum/care/controller/CareController.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/care/controller/CareController.java
@@ -1,7 +1,26 @@
 package com.ukcorp.ieum.care.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ukcorp.ieum.care.dto.CareRequestDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/care")
 public class CareController {
+
+    @GetMapping("/{care-id}")
+    public ResponseEntity getCareInfo(@PathVariable("care-id") Long careId) {
+        // TODO : 피보호자 정보 조회 로직 추가
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity postCareInfo(@RequestBody CareRequestDto careRequestDto) {
+        // TODO : 피보호자 정보 등록 로직 추가
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    // TODO : 피보호자 정보 수정
+    // TODO : 피보호자 정보 삭제
 }
