5fbabd1a2d718807d3ed63519e37626c998b4045
우혁 <dngur12kr@naver.com>
2024-01-19 15:50:22 +0900

    :sparkles: feat "라우터 추가 및 헤더푸터 연결"

---
+++ frontend/ieum/eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier/skip-formatting",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    // 들여쓰기 깊이 제한
+    "max-depth": ["error", 2],
+    // 함수의 매개변수 개수 제한
+    "max-params": ["error", 3],
+    // 함수의 길이 제한
+    "max-lines-per-function": ["error", { max: 10 }],
+  },
+};
