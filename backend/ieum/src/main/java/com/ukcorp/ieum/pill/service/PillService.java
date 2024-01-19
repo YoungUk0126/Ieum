b5733dd08554408993e43446082db6b587551dbc
김영욱 <dmnion@naver.com>
2024-01-19 16:26:00 +0900

    :sparkles: [feat] 약 정보 등록 기본 api 구현 약 정보 등록을 위한 DTO, REPO, Service, Controller 구현

--- backend/ieum/src/main/java/com/ukcorp/ieum/pill/service/PillService.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/pill/service/PillService.java
@@ -1,4 +1,11 @@
 package com.ukcorp.ieum.pill.service;
 
+import com.ukcorp.ieum.pill.dto.request.PillInfoRequestDto;
+
+import java.util.List;
+
 public interface PillService {
+
+//    약 정보 등록
+    int insertPill(PillInfoRequestDto pillInfo, List<String> pillTimeDate, List<String> pillTimeTime);
 }
