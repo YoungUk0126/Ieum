cc7b8f4deacaf2640b1e0ab836b42414a781f9dd
jangtaesoo <use02042@naver.com>
2024-01-22 10:21:31 +0900

    :sparkles: [feat] Navbar 추가
    공용 컴포넌트 Navbar 추가

--- frontend/ieum/src/components/common/TheNavbar.vue
+++ frontend/ieum/src/components/common/TheNavbar.vue
@@ -1,43 +1,55 @@
-<script setup>
-import { ref, watch, onMounted, computed } from 'vue'
-
-</script>
+<script setup></script>
 
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <router-link to="/theMainViewVue"><a class="navbar-brand" href="#">Navbar</a></router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <router-link to="/main"><a class="navbar-brand" href="#">Navbar</a></router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/chat"
+              ><a class="nav-link active" aria-current="page" href="#">대화목록</a></router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link to="/info">
+              <a class="nav-link" href="#">부모님 정보</a>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/schedule">
+              <a class="nav-link" href="#">일정</a>
+            </router-link>
+          </li>
+        </ul>
+
+        <!-- Use ms-auto to move the button group to the right -->
+        <div class="ms-auto">
+          <div class="d-grid gap-6 d-flex">
+            <div class="card">
+              <button class="btn btn-primary me-md-2" type="button">Button</button>
+              <button class="btn btn-primary" type="button">Button</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <style scoped>
-
-
+.navbar {
+  background-color: rgb(235, 251, 247) !important;
+}
 </style>
