5fd44fa39637ee61f522ad583dee56fa758c7cde
DevDachan <dachan9524@gmail.com>
2024-01-22 21:19:42 +0900

    :sparkles: [feat] 일정 컨트롤러 생성
    일정 api 컨트롤러 생성 및 get예시 작성

--- backend/ieum/src/main/java/com/ukcorp/ieum/temporalEvent/controller/TemporalEventController.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/temporalEvent/controller/TemporalEventController.java
@@ -1,12 +1,12 @@
-package com.ukcorp.ieum.schedule.controller;
+package com.ukcorp.ieum.temporalEvent.controller;
 
-import com.ukcorp.ieum.schedule.dto.TemporalEventDto;
-import com.ukcorp.ieum.schedule.service.TemporalEventService;
+import com.ukcorp.ieum.temporalEvent.dto.TemporalEventDto;
+import com.ukcorp.ieum.temporalEvent.service.TemporalEventService;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +21,7 @@ public class TemporalEventController {
 
     private final TemporalEventService temporalEventService;
 
+    @GetMapping("/")
     public  ResponseEntity<Map<String, Object>> getEvent(@RequestParam Long careNo){
       try{
         List<TemporalEventDto> list = temporalEventService.getList(careNo);
