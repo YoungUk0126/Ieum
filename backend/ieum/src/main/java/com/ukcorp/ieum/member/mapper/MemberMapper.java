a1449c1fd7d6fd2dd987a25dbe852e1a2bedee40
강다솔 <dasol199@naver.com>
2024-01-22 16:41:27 +0900

    :sparkles: [feat] MemberMapper 추가

---
+++ backend/ieum/src/main/java/com/ukcorp/ieum/member/mapper/MemberMapper.java
@@ -0,0 +1,20 @@
+package com.ukcorp.ieum.member.mapper;
+
+import com.ukcorp.ieum.member.dto.MemberDto;
+import com.ukcorp.ieum.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
+
+    @Mapping(target = "memberName", source = "name")
+    @Mapping(target = "memberPassword", source = "password")
+    @Mapping(target = "memberEmail", source = "email")
+    @Mapping(target = "memberPhone", source = "phone")
+    @Mapping(target = "memberNo", ignore = true)
+    @Mapping(target = "careInfo", ignore = true)
+    @Mapping(target = "serialCode", ignore = true)
+    @Mapping(target = "withdrawal", ignore = true)
+    Member memberDtoToMember(MemberDto member);
+}
