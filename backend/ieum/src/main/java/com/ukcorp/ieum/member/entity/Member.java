83007c77f64d853525f0c069ed81a3ca782f051b
이성목 <demy176@naver.com>
2024-01-22 12:25:43 +0900

    :sparkles: [feat] Member 엔티티 작성

--- backend/ieum/src/main/java/com/ukcorp/ieum/member/entity/Member.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/member/entity/Member.java
@@ -1,29 +1,47 @@
 package com.ukcorp.ieum.member.entity;
 
+import com.ukcorp.ieum.care.entity.CareInfo;
 import jakarta.persistence.*;
 import lombok.*;
 
 
 @Getter
-@Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 @Entity
 @Table(name = "MEMBER")
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long memberNo;
-    private long careNo;
+    @Column(name = "MEMBER_NO")
+    private Long memberNo;
+
+    @JoinColumn(name = "CARE_NO")
+    @OneToOne(fetch = FetchType.LAZY)
+    private CareInfo careInfo;
+
+    @Column(name = "MEMBER_ID")
     private String memberId;
+
+    @Column(name = "MEMBER_PASSWORD")
     private String memberPassword;
+
+    @Column(name = "SERIAL_CODE")
     private String serialCode;
+
+    @Column(name = "MEMBER_NAME")
     private String memberName;
+
+    @Column(name = "MEMBER_EMAIL")
     private String memberEmail;
+
+    @Column(name = "MEMBER_PHONE")
     private String memberPhone;
-    private boolean withdrawal;
+
+    @Column(name = "WIRDRAWUL")
+    @Enumerated(EnumType.STRING)
+    private Withdrawal withdrawal;
 }
