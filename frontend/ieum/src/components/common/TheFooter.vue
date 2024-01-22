2b499af446b8686090b7abad38d009a8a76a8b7c
jangtaesoo <use02042@naver.com>
2024-01-22 10:20:00 +0900

    :sparkles: [feat] footer 추가
    공용 컴포넌트 footer 추가

--- frontend/ieum/src/components/common/TheFooter.vue
+++ frontend/ieum/src/components/common/TheFooter.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>
-    </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style scoped>
-
-
-</style>
+    <footer class="all">
+      <div class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+          <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-muted" @click="showPrivacyPolicy"
+              >개인정보처리방침</a
+            >
+          </li>
+        </ul>
+        <p class="text-center text-muted">© 2022 Young Uk Company, Inc</p>
+      </div>
+    </footer>
+  </template>
+  
+  <script setup>
+  import swal from 'sweetalert'
+  const showPrivacyPolicy = () => {
+    swal('여기에 내용')
+  } //이벤트 리스너. 누르면 alert창이 뜨도록 설정하는 매서드.
+  </script>
+  
+  <style scoped>
+  /* Footer가 항상 페이지의 최하단에 고정되도록 스타일 추가 */
+  footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+  
+  .all {
+    background-color: rgb(235, 251, 247);
+  }
+  </style>
+  
