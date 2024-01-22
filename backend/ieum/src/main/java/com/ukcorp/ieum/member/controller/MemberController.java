2ccec651678be790cba4373758d9db883bd5ede4
강다솔 <dasol199@naver.com>
2024-01-22 16:40:54 +0900

    :sparkles: [feat] Member 회원가입, 로그인 추가

--- backend/ieum/src/main/java/com/ukcorp/ieum/member/controller/MemberController.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/member/controller/MemberController.java
@@ -1,10 +1,14 @@
 package com.ukcorp.ieum.member.controller;
 
+import com.ukcorp.ieum.jwt.JwtFilter;
+import com.ukcorp.ieum.jwt.dto.JwtToken;
 import com.ukcorp.ieum.member.dto.LoginDto;
 import com.ukcorp.ieum.member.dto.MemberDto;
 import com.ukcorp.ieum.member.entity.Member;
 import com.ukcorp.ieum.member.service.MemberServiceImpl;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
@@ -18,57 +22,55 @@ import java.util.Map;
 public class MemberController {
 
     private final MemberServiceImpl memberService;
+
     @PostMapping("/join")
-    public ResponseEntity<Map<String, Object>> joinMember(@RequestBody MemberDto member) {
+    public ResponseEntity joinMember(@Valid @RequestBody MemberDto member) {
 
-//        result에 service 결과 받아올 것
-        int result = memberService.joinMember(member);
+        memberService.signup(member);
 
-        if(result != 1) return handleError(result);
-        else return handleSuccess(result);
+        return new ResponseEntity(HttpStatus.OK);
     }
-    @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> loginMember(@RequestBody LoginDto loginForm){
-        Member result = memberService.loginMember(loginForm.getMemberId(), loginForm.getMemberId());
 
-        if(result != null) {
-            return handleSuccess(result);
-        }
-        else {
-            return handleError(result);
-        }
+    @PostMapping("/login")
+    public ResponseEntity<JwtToken> loginMember(@RequestBody LoginDto loginDto) {
+        JwtToken jwtToken = memberService.login(loginDto);
+
+        // Header에  토큰 설정
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.ACCESS_TOKEN_HEADER, "Bearer " + jwtToken.getAccessToken());
+        httpHeaders.add(JwtFilter.REFRESH_TOKEN_HEADER, "Bearer " + jwtToken.getRefreshToken());
+
+        return new ResponseEntity<>(jwtToken, httpHeaders, HttpStatus.OK);
     }
 
     @PutMapping("/modify")
     public ResponseEntity<Map<String, Object>> updateMember(@RequestBody MemberDto member) {
-        int result = memberService.updateMember(member);
+        int result = memberService.modifyMember(member);
 
-        if(result != 0) {
+        if (result != 0) {
             return handleSuccess(result);
-        }
-        else {
+        } else {
             return handleError(result);
         }
     }
 
     @DeleteMapping("/{member-id}")
-    public ResponseEntity<Map<String, Object>> deleteMember(@PathVariable("member-id") String memberId){
+    public ResponseEntity<Map<String, Object>> deleteMember(@PathVariable("member-id") String memberId) {
         int result = memberService.deleteMember(memberId);
 
-        if(result != 0) {
+        if (result != 0) {
             return handleSuccess(result);
-        }
-        else {
+        } else {
             return handleError(result);
         }
     }
 
 
     @GetMapping("/check-id/{member-id}")
-    public ResponseEntity<Map<String, Object>> searchMemberById(@PathVariable("member-id")String checkId){
+    public ResponseEntity<Map<String, Object>> searchMemberById(@PathVariable("member-id") String checkId) {
         Member result = memberService.findById(checkId);
 //        이미 아이디가 존재한다면
-        if(result != null){
+        if (result != null) {
             return handleError(0);
         }
 //        아이디가 없다면
@@ -76,7 +78,6 @@ public class MemberController {
             return handleSuccess(1);
         }
     }
-
 
 
     private ResponseEntity<Map<String, Object>> handleSuccess(Object data) {
