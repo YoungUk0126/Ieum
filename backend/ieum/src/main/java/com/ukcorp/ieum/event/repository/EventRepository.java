0e08728d5f21cf113308434ec77931ec7ca60352
이성목 <demy176@naver.com>
2024-01-22 12:32:23 +0900

    :truck: [rename] Event 엔티티 이름 변경
    Event가 SQL 예약어인 관계로 ReguralEvent로 변경

--- backend/ieum/src/main/java/com/ukcorp/ieum/event/repository/EventRepository.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/event/repository/EventRepository.java
@@ -1,9 +1,9 @@
 package com.ukcorp.ieum.event.repository;
 
-import com.ukcorp.ieum.event.entity.Event;
+import com.ukcorp.ieum.event.entity.ReguralEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<ReguralEvent, Long> {
 }
