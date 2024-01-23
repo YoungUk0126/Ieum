8dc2398f96638cf98b7e2350a946804f645301b6
강다솔 <dasol199@naver.com>
2024-01-23 13:49:07 +0900

    :sparkles: [feat] DB에 저장되는 비밀번호 암호화 추가

--- backend/ieum/src/main/java/com/ukcorp/ieum/member/mapper/MemberMapper.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/member/mapper/MemberMapper.java
@@ -4,12 +4,15 @@ import com.ukcorp.ieum.member.dto.MemberDto;
 import com.ukcorp.ieum.member.entity.Member;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
 
     @Mapping(target = "memberName", source = "name")
-    @Mapping(target = "memberPassword", source = "password")
+    @Mapping(target = "memberPassword", source = "password", qualifiedByName = "encryptPassword")
     @Mapping(target = "memberEmail", source = "email")
     @Mapping(target = "memberPhone", source = "phone")
     @Mapping(target = "memberNo", ignore = true)
@@ -18,4 +21,9 @@ public interface MemberMapper {
     @Mapping(target = "withdrawal", ignore = true)
     @Mapping(target = "authorities", ignore = true)
     Member memberDtoToMember(MemberDto member);
+
+    @Named("encryptPassword")
+    default String encryptPassword(String password) {
+        return new BCryptPasswordEncoder().encode(password);
+    }
 }
