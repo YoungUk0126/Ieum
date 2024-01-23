1f57820bc3b2f4a634d39de547d7866df90d99e2
강다솔 <dasol199@naver.com>
2024-01-23 10:19:32 +0900

    :sparkles: [feat] 로그인 시 DB에서 권한정보 뽑는 로직 추가

--- backend/ieum/src/main/java/com/ukcorp/ieum/member/service/MemberDetailsService.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/member/service/MemberDetailsService.java
@@ -3,6 +3,9 @@ package com.ukcorp.ieum.member.service;
 import com.ukcorp.ieum.member.entity.Member;
 import com.ukcorp.ieum.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,8 +13,14 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
@@ -26,9 +35,16 @@ public class MemberDetailsService implements UserDetailsService {
 
     // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 return
     private UserDetails createMemberDetail(Member member) {
+        Set<GrantedAuthority> grantedAuthorities = member.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.name()))
+                .collect(Collectors.toSet());
+
+        log.debug("토큰 생성할 때의 User 권한 >> " + grantedAuthorities);
+
         return User.builder()
                 .username(member.getMemberId())
                 .password(passwordEncoder.encode(member.getMemberPassword()))
+                .authorities(grantedAuthorities)
                 .build();
     }
 }
