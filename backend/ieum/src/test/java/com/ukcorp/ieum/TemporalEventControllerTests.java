ed5e13cae02a865e995f20a2ce6ac4455955ebb7
서다찬 <chn7894@naver.com>
2024-01-23 12:51:57 +0900

    ✅ [test] TemporalEventController 테스트 코드 추가
    기본 템플릿 추가 -> 이후 상세 구현 필요

---
+++ backend/ieum/src/test/java/com/ukcorp/ieum/TemporalEventControllerTests.java
@@ -0,0 +1,94 @@
+package com.ukcorp.ieum;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Slf4j
+@AutoConfigureMockMvc
+class TemporalEventControllerTests {
+
+  @Autowired
+  private MockMvc mock;
+
+  @Test
+  void build() {
+    log.debug("Success Build");
+  }
+
+  @Test
+  @DisplayName("일정 등록")
+  void testRegist() throws Exception {
+
+    String requestBody = "{\"eventNo\": \"18\", " +
+            "\"careNo\": \"17\", " +
+            "\"eventName\": \"경로당가기\"" +
+            "\"eventDate\": \"2024-01-15\"" +
+            "}";
+
+    mock.perform(put("/api/event")
+                    .content(requestBody)
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect((content().contentType(MediaType.APPLICATION_JSON))).andDo(print());
+  }
+
+  @Test
+  @DisplayName("일정 삭제")
+  void testDelete() throws Exception {
+
+    MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+    map.add("eventNo", "17");
+
+    mock.perform(delete("/api/event").params(map))
+            .andExpect(status().isOk())
+            .andExpect((content().contentType(MediaType.APPLICATION_JSON))).andDo(print());
+  }
+
+
+  @Test
+  @DisplayName("일정 리스트 조회")
+  void testSelect() throws Exception {
+
+    MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+    map.add("careNo", "18");
+
+    mock.perform(get("/api/event").params(map))
+            .andExpect(status().isOk())
+            .andExpect((content().contentType(MediaType.APPLICATION_JSON))).andDo(print());
+  }
+
+
+  @Test
+  @DisplayName("일정 수정")
+  void testUpdate() throws Exception {
+
+    String requestBody = "{\"eventNo\": \"18\", " +
+                           "\"careNo\": \"17\", " +
+                          "\"eventName\": \"경로당가기\"" +
+                          "\"eventDate\": \"2024-01-15\"" +
+                         "}";
+
+    mock.perform(post("/api/event")
+                    .content(requestBody)
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect((content().contentType(MediaType.APPLICATION_JSON))).andDo(print());
+  }
+
+
+
+}
