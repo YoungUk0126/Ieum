cbad1dfebde8330b66a43f47d5f0e1d5d28c345e
강다솔 <dasol199@naver.com>
2024-01-23 16:58:26 +0900

    :sparkles: [feat] Filter에서 만료된 토큰 다루는 로직 추가

--- backend/ieum/src/main/java/com/ukcorp/ieum/jwt/JwtFilter.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/jwt/JwtFilter.java
@@ -1,5 +1,6 @@
 package com.ukcorp.ieum.jwt;
 
+import com.ukcorp.ieum.exception.ExpiredTokenException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
@@ -8,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -21,7 +23,7 @@ import java.io.IOException;
 public class JwtFilter extends GenericFilterBean {
     public static final String ACCESS_TOKEN_HEADER = "Authorization";
     public static final String REFRESH_TOKEN_HEADER = "Authorization-refresh";
-    private TokenProvider tokenProvider;
+    private final TokenProvider tokenProvider;
 
     /**
      * JWT 토큰 인증정보 SecurityContext에 저장
@@ -40,15 +42,20 @@ public class JwtFilter extends GenericFilterBean {
         String accessToken = resolveToken(httpServletRequest);
         String requestURI = httpServletRequest.getRequestURI();
 
-        // 토큰 유효성 검사 진행
-        if (StringUtils.hasText(accessToken) && tokenProvider.validateToken(accessToken)) {
-            // 토큰이 정상적이라면 Authentication 객체 받아오기
-            Authentication authentication = tokenProvider.getAuthentication(accessToken);
-            // 받아온 Authentication 객체 SecurityContext에 저장
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            log.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
-        } else {
-            log.debug("유효한 JWT 토큰 또는 REFRESH 토큰이 없습니다, uri: {}", requestURI);
+        try {
+            // 토큰 유효성 검사 진행
+            if (StringUtils.hasText(accessToken) && tokenProvider.validateToken(accessToken)) {
+                // 토큰이 정상적이라면 Authentication 객체 받아오기
+                Authentication authentication = tokenProvider.getAuthentication(accessToken);
+                // 받아온 Authentication 객체 SecurityContext에 저장
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+            } else {
+                log.debug("유효한 JWT 토큰 또는 REFRESH 토큰이 없습니다, uri: {}", requestURI);
+            }
+        } catch (ExpiredTokenException e) {
+            // 만료된 토큰이라면 ExpiredTokenException을 catch
+            httpServletRequest.setAttribute("exception", "Expired Token");
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
