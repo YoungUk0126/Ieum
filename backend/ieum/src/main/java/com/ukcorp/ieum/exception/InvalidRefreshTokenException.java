e8483f6185c156ea8b05c7dde383c0cb74a36bad
강다솔 <dasol199@naver.com>
2024-01-24 13:03:04 +0900

    :sparkles: [feat] Token 관련 Exception 추가

--- backend/ieum/src/main/java/com/ukcorp/ieum/exception/InvalidRefreshTokenException.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/exception/InvalidRefreshTokenException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.FORBIDDEN)
-public class ExpiredTokenException extends RuntimeException{
+public class InvalidRefreshTokenException extends RuntimeException{
 
-    public ExpiredTokenException(String message) {
+    public InvalidRefreshTokenException(String message) {
         super(message);
     }
 
