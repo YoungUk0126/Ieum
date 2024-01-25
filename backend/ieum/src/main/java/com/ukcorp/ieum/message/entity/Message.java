84daf301893fa448adcdeb2b0ee71d5cadcf277d
서다찬 <chn7894@naver.com>
2024-01-25 16:57:37 +0900

    :sparkles: [feat] 음성/영상 메세지 엔티티 생성
    메세지 관련 엔티티 생성

---
+++ backend/ieum/src/main/java/com/ukcorp/ieum/message/entity/Message.java
@@ -0,0 +1,35 @@
+package com.ukcorp.ieum.message.entity;
+
+import com.ukcorp.ieum.care.entity.CareInfo;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "MESSAGE")
+public class Message {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "MESSAGE_NO")
+  private Long messageNo;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "CARE_NO")
+  private CareInfo careInfo;
+
+  @Column(name = "MESSAGE_NAME")
+  private String messageName;
+
+  @Column(name = "MESSAGE_TYPE")
+  private String messageType;
+
+  @Column(name = "SEND_DATE")
+  private LocalDate sendDate;
+
+}
