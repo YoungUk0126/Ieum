b66cdfadca7b235208b4d4c463c6cd7cb40f51d0
이성목 <demy176@naver.com>
2024-01-26 15:41:00 +0900

    :sparkles: [feat] IotService 인터페이스 완성

--- backend/ieum/src/main/java/com/ukcorp/ieum/iot/service/IotService.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/iot/service/IotService.java
@@ -1,4 +1,10 @@
 package com.ukcorp.ieum.iot.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface IotService {
+
+    void registDevice(String code);
+    void updateDeviceInfo(String code);
 }
