c4b1a36f71f4e59669607e7435c45e89528c8822
이성목 <demy176@naver.com>
2024-01-26 17:15:23 +0900

    :sparkles: [feat] IotService 구현 클래스 생성

---
+++ backend/ieum/src/main/java/com/ukcorp/ieum/iot/service/IotServiceImpl.java
@@ -0,0 +1,14 @@
+package com.ukcorp.ieum.iot.service;
+
+public class IotServiceImpl implements IotService {
+
+    @Override
+    public void registDevice(String memberId, String code) {
+
+    }
+
+    @Override
+    public void updateDeviceInfo(String memberId, String code) {
+
+    }
+}
