00eca3ef6754b941f8c366ea37864b41ba40d601
DasolKang <dasol199@naver.com>
2024-01-27 20:57:17 +0900

    :sparkles: [feat] DTO 이름 변경

--- backend/ieum/src/main/java/com/ukcorp/ieum/member/dto/MemberLoginRequestDto.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/member/dto/MemberLoginRequestDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class LoginDto {
+public class MemberLoginRequestDto {
     String memberId;
     String password;
 }
