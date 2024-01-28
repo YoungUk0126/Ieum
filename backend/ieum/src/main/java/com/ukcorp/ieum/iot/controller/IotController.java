29037ecca20031b0d7531819adf563910e80d29e
이성목 <demy176@naver.com>
2024-01-28 23:54:03 +0900

    :sparkles: [feat] 기기 관련 컨트롤러 구현

--- backend/ieum/src/main/java/com/ukcorp/ieum/iot/controller/IotController.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/iot/controller/IotController.java
@@ -1,21 +1,39 @@
 package com.ukcorp.ieum.iot.controller;
 
+import com.ukcorp.ieum.iot.service.IotService;
+import com.ukcorp.ieum.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/devices")
+@RequiredArgsConstructor
 public class IotController {
+
+    private final IotService iotService;
 
     @PostMapping
     public ResponseEntity postIotDevice(@RequestBody String code) {
-        // TODO : 이음이 코드 등록 로직 추가
+
+        String userId = JwtUtil.getUserId().get();
+
+        iotService.registSerialCode(code, userId);
+
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    @PutMapping
+    public ResponseEntity putIotDevice(@RequestBody String code) {
+
+        String userId = JwtUtil.getUserId().get();
+
+        iotService.updateSerialCode(code, userId);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
     // TODO : 이음 코드 수정
 }
