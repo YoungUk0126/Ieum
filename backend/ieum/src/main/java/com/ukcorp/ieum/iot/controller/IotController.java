d21de80cf240fc0f732521535c2482657b6784ea
이성목 <demy176@naver.com>
2024-01-29 10:56:08 +0900

    :sparkles: [feat] 컨트롤러 예외처리 추가

--- backend/ieum/src/main/java/com/ukcorp/ieum/iot/controller/IotController.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/iot/controller/IotController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -14,26 +16,44 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class IotController {
 
-    private final IotService iotService;
+  private final IotService iotService;
 
-    @PostMapping
-    public ResponseEntity postIotDevice(@RequestBody String code) {
+  @PostMapping
+  public ResponseEntity postIotDevice(@RequestBody String code) {
 
-        String userId = JwtUtil.getUserId().get();
-
-        iotService.registSerialCode(code, userId);
-
-        return new ResponseEntity(HttpStatus.OK);
+    String userId = JwtUtil.getMemberId().get();
+    try {
+      iotService.registSerialCode(code, userId);
+      return handleSuccess();
+    } catch (Exception e) {
+      return handleError();
     }
+  }
 
-    @PutMapping
-    public ResponseEntity putIotDevice(@RequestBody String code) {
-
-        String userId = JwtUtil.getUserId().get();
-
-        iotService.updateSerialCode(code, userId);
-
-        return new ResponseEntity(HttpStatus.OK);
-    }
+  @PutMapping
+  public ResponseEntity putIotDevice(@RequestBody String code) {
     // TODO : 이음 코드 수정
+
+    String userId = JwtUtil.getMemberId().get();
+
+    try {
+      iotService.updateSerialCode(code, userId);
+      return handleSuccess();
+    } catch (Exception e) {
+      return handleError();
+    }
+  }
+
+  private ResponseEntity<Map<String, Object>> handleSuccess() {
+    Map<String, Object> result = new HashMap<>();
+    result.put("success", true);
+    return new ResponseEntity<>(result, HttpStatus.OK);
+  }
+
+  private ResponseEntity<Map<String, Object>> handleError() {
+    Map<String, Object> result = new HashMap<>();
+    result.put("success", false);
+    return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+  }
+
 }
