241a9f401b4906d18e5cc560a8f9bf72c327d6f5
강다솔 <dasol199@naver.com>
2024-01-29 14:40:06 +0900

    :white_check_mark: [test] 회원 수정 테스트 추가

--- backend/ieum/src/test/java/com/ukcorp/ieum/member/controller/MemberControllerTest.java
+++ backend/ieum/src/test/java/com/ukcorp/ieum/member/controller/MemberControllerTest.java
@@ -4,22 +4,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.ukcorp.ieum.member.dto.MemberLoginRequestDto;
 import com.ukcorp.ieum.member.dto.MemberRequestDto;
+import com.ukcorp.ieum.member.service.MemberService;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Slf4j
 @AutoConfigureMockMvc
+@Transactional
 class MemberControllerTest {
 
     @Autowired
@@ -31,13 +38,12 @@ class MemberControllerTest {
     void 회원가입_성공() throws Exception {
         MemberRequestDto signupTestMember = MemberRequestDto.builder()
                 .memberId("test123")
-                .password("password!!")
+                .password("password123!!")
                 .name("테스트")
                 .email("test@naver.com")
                 .phone("010-1234-5678").build();
 
         String requestBody = objectMapper.writeValueAsString(signupTestMember);
-        log.info(requestBody);
 
         mockMvc.perform(post("/api/member/join")
                         .content(requestBody)
@@ -47,20 +53,33 @@ class MemberControllerTest {
 
     @Test
     void 로그인_성공() throws Exception {
-        // given
         MemberLoginRequestDto loginMember = MemberLoginRequestDto.builder()
                 .memberId("test123")
-                .password("password!!").build();
+                .password("password123!!").build();
 
-        // when
         mockMvc.perform(post("/api/member/login")
                 .content(objectMapper.writeValueAsString(loginMember))
                 .contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
+    @WithMockUser(username = "newjoin", authorities = {"ROLE_USER"})
+    void 회원정보_업데이트() throws Exception {
+        MemberRequestDto updateMember = MemberRequestDto.builder()
+                .memberId("newjoin")
+                .password("change123!")
+                .name("변경")
+                .phone("010-1236-2394")
+                .email("change123@gmail.com").build();
+
+        mockMvc.perform(put("/api/member")
+                .content(objectMapper.writeValueAsString(updateMember))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
     void 아이디중복확인() throws Exception {
-        // given
         String checkId = "test123";
 
         mockMvc.perform(get("/api/member/check-id/"+checkId))
@@ -68,10 +87,8 @@ class MemberControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
     void 이메일중복확인() throws Exception {
         String requestBody = "{\"email\": \"test123@gmail.com\"}";
-        log.info(requestBody);
 
         mockMvc.perform(post("/api/member/check-email")
                 .content(requestBody)
@@ -80,12 +97,9 @@ class MemberControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
     void 핸드폰중복확인() throws Exception {
         String requestBody = "{\"phone\": \"010-1234-5234\"}";
 
-//        String requestBody = objectMapper.writeValueAsString(phone);
-        log.info(requestBody);
         mockMvc.perform(post("/api/member/check-phone")
                 .content(requestBody)
                 .contentType(MediaType.APPLICATION_JSON))
