6e1e1600810cdc54392aca5f045dee1c759b70aa
CocoIsCat <demy176@naver.com>
2024-01-29 13:08:39 +0900

    :lock: [chore] SECRET_KEY 외부 주입

--- iot/ieum/ieum/settings.py
+++ iot/ieum/ieum/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os, json
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!w4mr*-*1e$lhk33v!c+2=*(#&dn-*julkm9k_4kht0ssr#=)2'
+SECRET_KEY = os.path.join(BASE_DIR, 'secrets.json')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
