9b79200ecbd842a7a3117e6bc116139c70fbc085
김영욱 <dmnion@naver.com>
2024-01-31 14:22:12 +0900

    :sparkles: [feat] Care careNo JWT토큰에서 받아오기
    Image 받아와서 Care에 추가하는 로직 필요

--- backend/ieum/src/main/java/com/ukcorp/ieum/care/service/CareServiceImpl.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/care/service/CareServiceImpl.java
@@ -4,8 +4,10 @@ import com.ukcorp.ieum.care.dto.request.CareInsertRequestDto;
 import com.ukcorp.ieum.care.dto.request.CareUpdateRequestDto;
 import com.ukcorp.ieum.care.dto.response.CareGetResponseDto;
 import com.ukcorp.ieum.care.entity.CareInfo;
+import com.ukcorp.ieum.care.entity.Gender;
 import com.ukcorp.ieum.care.mapper.CareMapper;
 import com.ukcorp.ieum.care.repository.CareRepository;
+import com.ukcorp.ieum.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -37,9 +39,10 @@ public class CareServiceImpl implements CareService {
   }
 
   @Override
-  public CareGetResponseDto getCareInfo(Long careNo) throws Exception {
+  public CareGetResponseDto getCareInfo() throws Exception {
     try {
-//        조회할 피보호자 데이터가 있으면 Entity를 받아오고, 없다면 Exception을 날린다.
+      Long careNo = JwtUtil.getCareNo().orElseThrow(() -> new Exception("토큰에 CareNo가 없어요"));
+//      피보호자 정보가 있으면 가져오고 없으면 예외 처리
       CareInfo care = careRepository.findById(careNo).orElseThrow(() -> new NoSuchElementException("존재하지 않는 피보호자 데이터입니다."));
       return careMapper.careInfoToCareGetResponseDto(care);
     } catch (RuntimeException e) {
@@ -52,9 +55,20 @@ public class CareServiceImpl implements CareService {
   @Override
   public void updateCareInfo(CareUpdateRequestDto careDto) throws Exception {
     try {
+//      피보호자 정보가 있으면 가져오고 없으면 예외 처리
+      Long careNo = JwtUtil.getCareNo().orElseThrow(() -> new Exception("토큰에 CareNo가 없어요"));
 //        수정하려는 피보호자 데이터가 있는지 확인
-      CareInfo check = careRepository.findById(careDto.getCareNo()).orElseThrow(() -> new NoSuchElementException("존재하지 않는 피보호자 데이터입니다."));
-      CareInfo care = careMapper.CareUpdateRequestDtoToCareInfo(careDto);
+      if(!careRepository.existsById(careNo)){
+        throw new NoSuchElementException("피보호자 정보가 없습니다.");
+      }
+//      이미지는 가져와서 여기 빌더에 넣어!
+      CareInfo care = CareInfo.builder()
+              .careNo(careNo)
+              .careAddr(careDto.getCareAddr())
+              .careBirth(careDto.getCareBirth())
+              .careGender(Gender.valueOf(careDto.getCareGender()))
+              .carePhone(careDto.getCarePhone())
+              .build();
       careRepository.save(care);
     } catch (DataIntegrityViolationException e) {
       log.debug("수정 오류!");
@@ -64,8 +78,10 @@ public class CareServiceImpl implements CareService {
 
   @Transactional
   @Override
-  public void deleteCareInfo(Long careNo) throws Exception {
+  public void deleteCareInfo() throws Exception {
     try {
+//      피보호자 정보가 있으면 가져오고 없으면 예외 처리
+      Long careNo = JwtUtil.getCareNo().orElseThrow(() -> new Exception("토큰에 CareNo가 없어요"));
       careRepository.deleteById(careNo);
     } catch (EmptyResultDataAccessException e) {
       log.debug("삭제 오류!");
