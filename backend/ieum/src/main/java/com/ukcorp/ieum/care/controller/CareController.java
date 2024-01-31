0e41dad88002aae3fbf34426ac969710b83718f8
김영욱 <dmnion@naver.com>
2024-01-31 15:28:50 +0900

    :sparkles: [feat] iamge 저장하는 컨트롤러 작성
    테스트 및 컨펌 필요

--- backend/ieum/src/main/java/com/ukcorp/ieum/care/controller/CareController.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/care/controller/CareController.java
@@ -5,13 +5,19 @@ import com.ukcorp.ieum.care.dto.request.CareUpdateRequestDto;
 import com.ukcorp.ieum.care.dto.response.CareGetResponseDto;
 import com.ukcorp.ieum.care.service.CareServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +25,31 @@ import java.util.Map;
 @Slf4j
 public class CareController {
 
+
+
     private final CareServiceImpl careService;
+
+    @PostMapping(value = "/insertImage")
+    public ResponseEntity<Map<String,Object>> uploadImage(
+            @RequestParam("file") MultipartFile file
+    ) {
+        try {
+            String path = "src/main/resources/static";
+            // 파일 저장 디렉토리 경로 (폴더 내에 프론트 프로젝트 함께 존재해야 함)
+            String uploadDir = "../"+path+"/public/";
+            String randomId = UUID.randomUUID().toString();
+            // 파일 저장할 경로
+            String fileName = randomId + ".jpg";
+            Path filePath = Paths.get(uploadDir, fileName);
+            file.transferTo(filePath); // 파일 다운로드
+            return handleSuccess(randomId);
+
+        } catch (Exception e) {
+            // 저장 중 에러가 발생한 경우 반환할 응답
+            log.debug(e.getMessage());
+            return handleError("fail");
+        }
+    }
 
     /**
      * 피보호자의 Dto로 Entity에 저장하는 기능
