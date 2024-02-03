acf1761b750e5e2494c513bca3a60c56dd8b31e7
DevDachan <dachan9524@gmail.com>
2024-02-04 03:14:45 +0900

    :sparkles: [feat] 일정 상세 내용 디자인 변경

--- frontend/ieum/src/components/schedule/TheScheduleBody.vue
+++ frontend/ieum/src/components/schedule/TheScheduleBody.vue
@@ -26,6 +26,47 @@
           </div>
         </div>
       </div>
+      <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead
+          class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400"
+        >
+          <tr>
+            <th scope="col" class="px-6 py-3 rounded-s-lg">일정 내용</th>
+            <th scope="col" class="px-6 py-3 text-center">수정</th>
+            <th scope="col" class="px-6 py-3 text-center rounded-e-lg">삭제</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white dark:bg-gray-800">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              <input
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                value="Apple MacBook Pro 17"
+              />
+            </th>
+            <td class="px-6 py-4 text-center">
+              <button
+                type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                @click="edit"
+              >
+                수정
+              </button>
+            </td>
+            <td class="px-6 py-4 text-center">
+              <button
+                type="button"
+                class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              >
+                삭제
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -69,7 +110,7 @@ watch(
     schedule.value = []
     for (let i = 0; i < todos.value.length; i++) {
       if (nv.getTime() === todos.value[i].dates.getTime()) {
-        schedule.value.push(todos.value[i].description)
+        schedule.value.push(todos.value[i])
       }
     }
   }
@@ -89,7 +130,8 @@ const getApi = (year, month) => {
         description: element.eventName,
         isComplete: false,
         dates: new Date(element.eventDate),
-        color: 'orange'
+        color: 'orange',
+        eventNo: element.eventNo
       })
     })
   })
