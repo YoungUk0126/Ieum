58e57d4777b9a666681f0e112d2ecd3b63a1b1d7
이성목 <demy176@naver.com>
2024-02-04 17:14:44 +0900

    :heavy_plus_sign: [chore] 메시지 브로커용 redis 설정

--- backend/ieum/src/main/java/com/ukcorp/ieum/config/RedisConfig.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/config/RedisConfig.java
@@ -1,37 +1,81 @@
 package com.ukcorp.ieum.config;
 
+import com.ukcorp.ieum.socket.service.RedisSubscriber;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
 
     @Value("${spring.data.redis.port}")
-    private int port;
+    private int port1;
 
     @Value("${spring.data.redis.host}")
-    private String host;
+    private String host1;
+
+    @Value("${spring.data.redis.port2}")
+    private int port2;
+
+    @Value("${spring.data.redis.host2}")
+    private String host2;
+
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        return new LettuceConnectionFactory(host1, port1);
     }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
         // redisTemplate를 받아와서 set, get, delete를 사용
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         // setKeySerializer, setValueSerializer 설정
         // redis-cli을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
 
         return redisTemplate;
+    }
+
+    @Bean RedisConnectionFactory redisConnectionFactorySocket() {
+        return new LettuceConnectionFactory(host2, port2);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplateSocket() {
+        // redisTemplate를 받아와서 set, get, delete를 사용
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactorySocket());
+        // setKeySerializer, setValueSerializer 설정
+        // redis-cli을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Object>());
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory redisConnectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        return container;
+    }
+
+    @Bean
+    public ChannelTopic topic() {
+        return new ChannelTopic("topic");
     }
 }
