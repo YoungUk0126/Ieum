30e2827806d72193637f6b0da83e2865dfc604ab
이성목 <demy176@naver.com>
2024-02-05 01:17:00 +0900

    :sparkles: [feat] 소켓으로 전송/수신 서비스

---
+++ backend/ieum/src/main/java/com/ukcorp/ieum/socket/service/SocketService.java
@@ -0,0 +1,17 @@
+package com.ukcorp.ieum.socket.service;
+
+public interface SocketService {
+
+    void sendPillDataToIot(Long careNo);
+
+    void sendEventDataToIot(Long careNo);
+
+    void sendSleepDataToIot(Long careNo);
+
+    void sendMealDataToIot(Long careNo);
+
+    void sendMessageToIot(Long careNo);
+
+    void sendCallAlertToIot(Long careNo);
+
+}
