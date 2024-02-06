1d92a40b599c27233f895c8c966ba2552bc8ac35
강다솔 <dasol199@naver.com>
2024-02-06 15:48:32 +0900

    :white_check_mark: [test] Meal Controller 유닛테스트로 변경

--- backend/ieum/src/test/java/com/ukcorp/ieum/meal/controller/MealControllerTest.java
+++ backend/ieum/src/test/java/com/ukcorp/ieum/meal/controller/MealControllerTest.java
@@ -4,30 +4,45 @@ package com.ukcorp.ieum.meal.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.ukcorp.ieum.config.SecurityTestConfig;
 import com.ukcorp.ieum.meal.dto.request.MealInsertRequestDto;
 import com.ukcorp.ieum.meal.dto.request.MealUpdateRequestDto;
+import com.ukcorp.ieum.meal.dto.response.MealGetResponseDto;
+import com.ukcorp.ieum.meal.service.MealServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
+@WebMvcTest(MealController.class)
+@ExtendWith(MockitoExtension.class)
+@Import(SecurityTestConfig.class)
 @Slf4j
-@AutoConfigureMockMvc
 public class MealControllerTest {
 
     @Autowired
     private MockMvc mock;
-
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private MealServiceImpl mealService;
 
     @Test
     void build() {
@@ -36,6 +51,7 @@ public class MealControllerTest {
 
     @Test
     @DisplayName("약 정보 등록")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
     void testInsert() throws Exception {
         MealInsertRequestDto requestDto = MealInsertRequestDto.builder()
                 .mealTime1(70000L)
@@ -44,37 +60,45 @@ public class MealControllerTest {
                 .build();
 
         //    // JSON 변환
-        ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         String requestBody = objectMapper.writeValueAsString(requestDto);
 
-
-//        String requestBody = "{\"care_no\": \"1\", \"meal_time1\": \"90000\", \"meal_time2\": \"140000\", \"meal_time3\": \"210000\"}";
+        doNothing().when(mealService).insertMeal(any(MealInsertRequestDto.class));
 
         mock.perform(post("/api/meal")
                         .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)))
+                .andDo(print());
     }
 
     @Test
     @DisplayName("약 정보 조회")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
     void testSelect() throws Exception {
-//        Query Param 담는애(get)
-//        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-//        Query Param 변수 이름
-//        map.add("care-no", "1");
+        // given
+        MealGetResponseDto response = MealGetResponseDto.builder().build();
 
-//        get
-        mock.perform(get("/api/meal/1"))
+        when(mealService.getMeal()).thenReturn(response);
+
+        // get
+        mock.perform(get("/api/meal"))
                 .andExpect(status().isOk())
-                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.mealInfoNo").value(response.getMealInfoNo()))
+                .andExpect(jsonPath("$.data.careNo").value(response.getCareNo()))
+                .andExpect(jsonPath("$.data.mealTime1").value(response.getMealTime1()))
+                .andExpect(jsonPath("$.data.mealTime2").value(response.getMealTime2()))
+                .andExpect(jsonPath("$.data.mealTime3").value(response.getMealTime3()))
+                .andDo(print());
     }
 
     @Test
     @DisplayName("약 정보 수정")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
     void testUpdate() throws Exception {
         MealUpdateRequestDto requestDto = MealUpdateRequestDto.builder()
                 .mealInfoNo(2L)
@@ -84,24 +108,26 @@ public class MealControllerTest {
                 .build();
 
         //    // JSON 변환
-        ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         String requestBody = objectMapper.writeValueAsString(requestDto);
 
-//        String requestBody = "{\"care_no\": \"1\", \"meal_info_no\": \"1\", \"meal_time1\": \"90000\", \"meal_time2\": \"130000\", \"meal_time3\": \"200000\"}";
+        doNothing().when(mealService).updateMeal(any(MealUpdateRequestDto.class));
 
         mock.perform(put("/api/meal")
                         .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)))
+                .andDo(print());
     }
 
     @Test
     @DisplayName("약 정보 삭제")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
     void testDelete() throws Exception {
+        doNothing().when(mealService).deleteMeal(anyLong());
+
 //        delete
         mock.perform(delete("/api/meal/1"))
                 .andExpect(status().isOk())
