e284c247b2a01e08a483e7cedf063824f0de4ccd
강다솔 <dasol199@naver.com>
2024-02-06 15:47:52 +0900

    :white_check_mark: [test] Pill Controller 유닛 테스트로 변경

--- backend/ieum/src/test/java/com/ukcorp/ieum/PillControllerTest.java
+++ backend/ieum/src/test/java/com/ukcorp/ieum/PillControllerTest.java
@@ -3,159 +3,177 @@ package com.ukcorp.ieum;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.ukcorp.ieum.config.SecurityTestConfig;
+import com.ukcorp.ieum.event.controller.EventController;
+import com.ukcorp.ieum.event.dto.response.EventGetResponseDto;
+import com.ukcorp.ieum.event.service.EventServiceImpl;
+import com.ukcorp.ieum.pill.controller.PillController;
 import com.ukcorp.ieum.pill.dto.request.PillInfoInsertRequestDto;
 import com.ukcorp.ieum.pill.dto.request.PillInfoUpdateRequestDto;
 import com.ukcorp.ieum.pill.dto.request.PillTimeInsertRequestDto;
 import com.ukcorp.ieum.pill.dto.request.PillTimeUpdateRequestDto;
+import com.ukcorp.ieum.pill.dto.response.PillInfoJoinResponseDto;
+import com.ukcorp.ieum.pill.dto.response.PillTimeGetResponseDto;
+import com.ukcorp.ieum.pill.service.PillServiceImpl;
+import com.ukcorp.ieum.socket.dto.response.PillResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
+@WebMvcTest(PillController.class)
+@ExtendWith(MockitoExtension.class)
+@Import(SecurityTestConfig.class)
 @Slf4j
-@AutoConfigureMockMvc
 public class PillControllerTest {
 
-  @Autowired
-  private MockMvc mock;
+    @Autowired
+    private MockMvc mock;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private PillServiceImpl pillService;
 
-  @Test
-  void build() {
-    log.debug("Success Build");
-  }
+    @Test
+    void build() {
+        log.debug("Success Build");
+    }
 
-  @Test
-  @DisplayName("약 정보 등록")
-  void testRegist() throws Exception {
+    @Test
+    @DisplayName("약 정보 등록")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
+    void testRegist() throws Exception {
+        // 약 정보 등록 관련 테스트
+        PillInfoInsertRequestDto requestDto = PillInfoInsertRequestDto.builder().build();
+        List<PillTimeInsertRequestDto> pillTimes = new ArrayList<>();
+        requestDto.setPillTimes(pillTimes);
 
-    // 약 정보 등록 관련 테스트
-    PillInfoInsertRequestDto requestDto = new PillInfoInsertRequestDto();
-    requestDto.setPillName("이름");
-    requestDto.setPillStartDate(LocalDate.parse("2024-01-30"));
-    requestDto.setPillEndDate(LocalDate.parse("2024-02-29"));
-    requestDto.setPillMethod("식전");
-    requestDto.setPillDate("1111111");
+        // JSON 변환
+        String requestBody = objectMapper.writeValueAsString(requestDto);
 
-    List<PillTimeInsertRequestDto> pillTimes = new ArrayList<>();
-    PillTimeInsertRequestDto pillTime1 = new PillTimeInsertRequestDto();
-    pillTime1.setPillTakeTime(90000L);
-    pillTimes.add(pillTime1);
+        doNothing().when(pillService).insertPill(requestDto);
 
-    PillTimeInsertRequestDto pillTime2 = new PillTimeInsertRequestDto();
-    pillTime2.setPillTakeTime(140000L);
-    pillTimes.add(pillTime2);
+        mock.perform(post("/api/pill")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+    }
 
-    requestDto.setPillTimes(pillTimes);
+    @Test
+    @DisplayName("약 정보 삭제")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
+    void testDelete() throws Exception {
+        doNothing().when(pillService).deletePillInfo(anyLong());
 
-//    // JSON 변환
-    ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.registerModule(new JavaTimeModule());
-    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    String requestBody = objectMapper.writeValueAsString(requestDto);
-//    String requestBody = "{\"care_no\": \"1\",\"pill_name\":\"이름\",\"pill_start_date\":\"2024-01-30\",\"pill_end_date\":\"2024-02-29\",\"pill_method\":\"식전\",\"pill_date\":\"1111111\",\"pill_times\":[{\"pill_take_time\":\"90000\"},{\"pill_take_time\":\"140000\"}]}";
+        // 1에 해당하는 pillInfo 삭제
+        mock.perform(delete("/api/pill/2"))
+                .andExpect(status().isOk())
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+    }
 
-    mock.perform(post("/api/pill")
-                    .content(requestBody)
-                    .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-  }
+    @Test
+    @DisplayName("약 복용 시간 정보 삭제")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
+    void testTimeDelete() throws Exception {
+        doNothing().when(pillService).deletePillTime(anyLong());
 
-  @Test
-  @DisplayName("약 정보 삭제")
-  void testDelete() throws Exception {
-
-    // 1에 해당하는 pillInfo 삭제
-    mock.perform(delete("/api/pill/2"))
-            .andExpect(status().isOk())
-            .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-  }
-
-  @Test
-  @DisplayName("약 복용 시간 정보 삭제")
-  void testTimeDelete() throws Exception {
-
-    // 1에 해당하는 pillTime 삭제
-    mock.perform(delete("/api/pill/time/4"))
-            .andExpect(status().isOk())
-            .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-  }
+        // 1에 해당하는 pillTime 삭제
+        mock.perform(delete("/api/pill/time/4"))
+                .andExpect(status().isOk())
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+    }
 
 
-  @Test
-  @DisplayName("약 정보 리스트 조회")
-  void testSelectList() throws Exception {
+    @Test
+    @DisplayName("약 정보 리스트 조회")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
+    void testSelectList() throws Exception {
+        // given
+        List<PillInfoJoinResponseDto> response = new ArrayList<>();
+        response.add(PillInfoJoinResponseDto.builder().build());
 
-    // careNo가 1인 사용자의 일정 리스트 조회
-    mock.perform(get("/api/pill/1"))
-            .andExpect(status().isOk())
-            .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-  }
+        Mockito.when(pillService.getAllPillInfo()).thenReturn(response);
 
-  @Test
-  @DisplayName("약 정보 상세 조회")
-  void testSelectDetail() throws Exception {
-    // eventNo으로 해당 일정 상세 조회
-    mock.perform(get("/api/pill/detail/3"))
-            .andExpect(status().isOk())
-            .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-  }
+        // careNo가 1인 사용자의 일정 리스트 조회
+        mock.perform(get("/api/pill"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data[0].pillInfoNo").value(response.get(0).getPillInfoNo()))
+                .andExpect(jsonPath("$.data[0].pillName").value(response.get(0).getPillName()))
+                .andExpect(jsonPath("$.data[0].pillStartDate").value(response.get(0).getPillStartDate()))
+                .andExpect(jsonPath("$.data[0].pillEndDate").value(response.get(0).getPillEndDate()))
+                .andExpect(jsonPath("$.data[0].pillMethod").value(response.get(0).getPillMethod()))
+                .andExpect(jsonPath("$.data[0].pillDate").value(response.get(0).getPillDate()))
+                .andExpect(jsonPath("$.data[0].pillTimes").value(response.get(0).getPillTimes()))
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+    }
 
+    @Test
+    @DisplayName("약 정보 상세 조회")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
+    void testSelectDetail() throws Exception {
+        // given
+        PillInfoJoinResponseDto response = PillInfoJoinResponseDto.builder().build();
 
-  @Test
-  @DisplayName("일정 수정")
-  void testUpdate() throws Exception {
+        Mockito.when(pillService.getPillInfo(anyLong())).thenReturn(response);
 
-    // 약 정보 등록 관련 테스트
-    PillInfoUpdateRequestDto requestDto = new PillInfoUpdateRequestDto();
-    requestDto.setPillInfoNo(1L);
-    requestDto.setPillName("이름 수정");
-    requestDto.setPillStartDate(LocalDate.parse("2024-01-30"));
-    requestDto.setPillEndDate(LocalDate.parse("2024-02-29"));
-    requestDto.setPillMethod("식후");
-    requestDto.setPillDate("0000000");
-
-    List<PillTimeUpdateRequestDto> pillTimes = new ArrayList<>();
-    PillTimeUpdateRequestDto pillTime1 = new PillTimeUpdateRequestDto();
-    pillTime1.setPillInfoNo(1L);
-    pillTime1.setPillTakeTime(70000L);
-    pillTime1.setPillTimeNo(1L);
-    pillTimes.add(pillTime1);
-
-    PillTimeUpdateRequestDto pillTime2 = new PillTimeUpdateRequestDto();
-    pillTime2.setPillInfoNo(1L);
-    pillTime2.setPillTakeTime(120000L);
-    pillTime2.setPillTimeNo(2L);
-    pillTimes.add(pillTime2);
-
-    requestDto.setPillTimes(pillTimes);
+        // eventNo으로 해당 일정 상세 조회
+        mock.perform(get("/api/pill/detail/3"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.pillInfoNo").value(response.getPillInfoNo()))
+                .andExpect(jsonPath("$.data.pillName").value(response.getPillName()))
+                .andExpect(jsonPath("$.data.pillStartDate").value(response.getPillStartDate()))
+                .andExpect(jsonPath("$.data.pillEndDate").value(response.getPillEndDate()))
+                .andExpect(jsonPath("$.data.pillMethod").value(response.getPillMethod()))
+                .andExpect(jsonPath("$.data.pillDate").value(response.getPillDate()))
+                .andExpect(jsonPath("$.data.pillTimes").value(response.getPillTimes()))
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+    }
 
 
-//    // JSON 변환
-    ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.registerModule(new JavaTimeModule());
-    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    String requestBody = objectMapper.writeValueAsString(requestDto);
+    @Test
+    @DisplayName("일정 수정")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
+    void testUpdate() throws Exception {
 
-    mock.perform(put("/api/pill")
-                    .content(requestBody)
-                    .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-  }
+        // 약 정보 등록 관련 테스트
+        PillInfoUpdateRequestDto requestDto = new PillInfoUpdateRequestDto();
+        List<PillTimeUpdateRequestDto> pillTimes = new ArrayList<>();
+        requestDto.setPillTimes(pillTimes);
 
+        // JSON 변환
+        String requestBody = objectMapper.writeValueAsString(requestDto);
+
+        doNothing().when(pillService).updatePill(requestDto);
+
+        mock.perform(put("/api/pill")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+    }
 }
