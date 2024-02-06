3c17a1dc01d01e9eb8e6cad8f0da1a4f9bc02d87
강다솔 <dasol199@naver.com>
2024-02-06 15:51:28 +0900

    :white_check_mark: [test] TemporalEventController 유닛 테스트로 변경

--- backend/ieum/src/test/java/com/ukcorp/ieum/TemporalEventControllerTests.java
+++ backend/ieum/src/test/java/com/ukcorp/ieum/TemporalEventControllerTests.java
@@ -1,99 +1,144 @@
 package com.ukcorp.ieum;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ukcorp.ieum.config.SecurityTestConfig;
+import com.ukcorp.ieum.pill.controller.PillController;
+import com.ukcorp.ieum.pill.dto.response.PillInfoJoinResponseDto;
+import com.ukcorp.ieum.pill.service.PillServiceImpl;
+import com.ukcorp.ieum.temporalEvent.controller.TemporalEventController;
+import com.ukcorp.ieum.temporalEvent.dto.request.TemporalEventInsertRequestDto;
+import com.ukcorp.ieum.temporalEvent.dto.request.TemporalEventUpdateRequestDto;
+import com.ukcorp.ieum.temporalEvent.dto.response.TemporalEventResponseDto;
+import com.ukcorp.ieum.temporalEvent.service.TemporalEventService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
+@WebMvcTest(TemporalEventController.class)
+@ExtendWith(MockitoExtension.class)
+@Import(SecurityTestConfig.class)
 @Slf4j
-@AutoConfigureMockMvc
 class TemporalEventControllerTests {
 
-  @Autowired
-  private MockMvc mock;
+    @Autowired
+    private MockMvc mock;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private TemporalEventService temporalEventService;
 
-  @Test
-  void build() {
-    log.debug("Success Build");
-  }
+    @Test
+    void build() {
+        log.debug("Success Build");
+    }
 
-  @Test
-  @DisplayName("일정 등록")
-  void testRegist() throws Exception {
+    @Test
+    @DisplayName("일정 등록")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
+    void testRegist() throws Exception {
+        // 일정 등록 관련 테스트
+        TemporalEventInsertRequestDto requestDto = TemporalEventInsertRequestDto.builder().build();
+        String requestBody = objectMapper.writeValueAsString(requestDto);
 
-    // 일정 등록 관련 테스트
-    String requestBody = "{" +
-            "\"care_no\": \"1\", " +
-            "\"event_name\": \"경로당가기\"," +
-            "\"event_date\": \"2024-01-15\"" +
-            "}";
+        doNothing().when(temporalEventService).registEvent(requestDto);
 
-    mock.perform(post("/api/event")
-                    .content(requestBody)
-                    .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-  }
+        mock.perform(post("/api/event")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+    }
 
-  @Test
-  @DisplayName("일정 삭제")
-  void testDelete() throws Exception {
+    @Test
+    @DisplayName("일정 삭제")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
+    void testDelete() throws Exception {
+        doNothing().when(temporalEventService).deleteEvent(anyLong());
 
-    // 17에 해당하는 eventNo 일정 삭제
-    mock.perform(delete("/api/event/1"))
-            .andExpect(status().isOk())
-            .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-  }
+        // 17에 해당하는 eventNo 일정 삭제
+        mock.perform(delete("/api/event/1"))
+                .andExpect(status().isOk())
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+    }
 
+    @Test
+    @DisplayName("일정 리스트 조회")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
+    void testSelect() throws Exception {
+        // given
+        List<TemporalEventResponseDto> response = new ArrayList<>();
+        response.add(TemporalEventResponseDto.builder().build());
 
-  @Test
-  @DisplayName("일정 리스트 조회")
-  void testSelect() throws Exception {
-    
-    // careNo이 17인 사용자의 일정 리스트 조회
-    mock.perform(get("/api/event/1"))
-            .andExpect(status().isOk())
-            .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-  }
+        Mockito.when(temporalEventService.getList()).thenReturn(response);
 
-  @Test
-  @DisplayName("일정 상세 조회")
-  void testSelectDetail() throws Exception {
-    // eventNo으로 해당 일정 상세 조회
-    mock.perform(get("/api/event/detail/1"))
-            .andExpect(status().isOk())
-            .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-  }
+        // 사용자의 일정 리스트 조회
+        mock.perform(get("/api/event"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data[0].eventNo").value(response.get(0).getEventNo()))
+                .andExpect(jsonPath("$.data[0].careNo").value(response.get(0).getCareNo()))
+                .andExpect(jsonPath("$.data[0].eventName").value(response.get(0).getEventName()))
+                .andExpect(jsonPath("$.data[0].eventDate").value(response.get(0).getEventDate()))
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+    }
 
+    @Test
+    @DisplayName("일정 상세 조회")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
+    void testSelectDetail() throws Exception {
+        // given
+        TemporalEventResponseDto response = TemporalEventResponseDto.builder().build();
 
+        Mockito.when(temporalEventService.getDetail(anyLong())).thenReturn(response);
 
-  @Test
-  @DisplayName("일정 수정")
-  void testUpdate() throws Exception {
+        // eventNo으로 해당 일정 상세 조회
+        mock.perform(get("/api/event/detail/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.eventNo").value(response.getEventNo()))
+                .andExpect(jsonPath("$.data.careNo").value(response.getCareNo()))
+                .andExpect(jsonPath("$.data.eventName").value(response.getEventName()))
+                .andExpect(jsonPath("$.data.eventDate").value(response.getEventDate()))
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+    }
 
-    String requestBody = "{\"event_no\": \"4\", " +
-            "\"care_no\": \"1\", " +
-            "\"event_name\": \"경로당22가기\"," +
-            "\"event_date\": \"2024-01-15\"" +
-            "}";
+    @Test
+    @DisplayName("일정 수정")
+    @WithMockUser(username = "test123", authorities = {"ROLE_USER"})
+    void testUpdate() throws Exception {
+        TemporalEventUpdateRequestDto requestDto = TemporalEventUpdateRequestDto.builder().build();
+        String requestBody = objectMapper.writeValueAsString(requestDto);
 
-    mock.perform(put("/api/event")
-                    .content(requestBody)
-                    .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
-  }
+        doNothing().when(temporalEventService).modifyEvent(requestDto);
 
-
+        mock.perform(put("/api/event")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect((content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))).andDo(print());
+    }
 
 }
