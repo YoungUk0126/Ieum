31e66f2a739a41cbde2ed17fe8a96de05d61072e
DevDachan <dachan9524@gmail.com>
2024-02-09 00:42:19 +0900

    :sparkles: [feat] header 수정

--- frontend/ieum/src/components/common/TheHeader.vue
+++ frontend/ieum/src/components/common/TheHeader.vue
@@ -2,52 +2,45 @@
   <nav
     class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600"
   >
-    <div class="w-full flex flex-wrap items-center justify-between mx-auto p-4">
-      <div class="flex">
-        <router-link
-          :to="{ name: 'TheMainViewVue' }"
-          class="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img src="@/assets/images/logo.png" class="h-16" />
-        </router-link>
-      </div>
-      <div
-        v-show="!hide"
-        class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse mr-5"
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <router-link
+        :to="{ name: 'TheMainViewVue' }"
+        class="flex items-center space-x-3 rtl:space-x-reverse"
       >
-        <button
-          ref="hamb"
-          data-collapse-toggle="navbar-user"
-          type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-user"
-          aria-expanded="false"
+        <img src="@/assets/images/logo.png" class="h-16" />
+      </router-link>
+      <button
+        ref="hamb"
+        data-collapse-toggle="navbar-user"
+        type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-user"
+        aria-expanded="false"
+      >
+        <span class="sr-only">Open main menu</span>
+        <svg
+          class="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
         >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-      </div>
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15"
+          />
+        </svg>
+      </button>
       <div
         v-show="!hide"
         class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
         id="navbar-user"
       >
         <ul
-          class="flex flex-col font-bold p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0"
+          class="flex flex-col font-bold text-right space-y-2 p-4 md:p-0 mt-0 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0"
         >
           <li>
             <router-link
@@ -84,6 +77,14 @@
               class="menu-block text-black rounded md:bg-transparent md:text-green-500 md:p-0 md:hover:text-green-600"
               >메세지</router-link
             >
+          </li>
+          <li>
+            <button
+              @click="logout"
+              class="menu-block text-black rounded md:bg-transparent md:text-green-500 md:p-0 md:hover:text-green-600"
+            >
+              로그아웃
+            </button>
           </li>
         </ul>
       </div>
