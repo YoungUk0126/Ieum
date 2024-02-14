fab21b85990cd08ebdf27a1d7f959bc7f5137602
CocoIsCat <demy176@naver.com>
2024-02-14 09:21:25 +0900

    :sparkles: [feat] 통화 종료 메시지 전달

--- backend/ieum/src/main/java/com/ukcorp/ieum/iot/controller/IotController.java
+++ backend/ieum/src/main/java/com/ukcorp/ieum/iot/controller/IotController.java
@@ -1,10 +1,15 @@
 package com.ukcorp.ieum.iot.controller;
 
+import com.ukcorp.ieum.care.dto.response.CareGetResponseDto;
+import com.ukcorp.ieum.care.service.CareService;
 import com.ukcorp.ieum.iot.dto.SerialRequestDto;
 import com.ukcorp.ieum.iot.service.IotService;
 import com.ukcorp.ieum.jwt.JwtUtil;
+
 import java.util.HashMap;
 import java.util.Map;
+
+import com.ukcorp.ieum.socket.service.SocketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,45 +25,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class IotController {
 
-    private final IotService iotService;
+  private final IotService iotService;
+  private final SocketService socketService;
 
-    @PostMapping("/fire")
-    public ResponseEntity<Map<String, Object>> sendFireAlert(@RequestBody SerialRequestDto serial) {
-        try {
-            iotService.sendFireSms(serial.getSerial());
-            return handleSuccess();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return handleError();
-        }
-    }
+  @GetMapping("/colseAlert")
+  public void sendCloseAlert() {
+    iotService.sendCloseAlert();
+  }
 
-    @PostMapping("/check-info")
-    public ResponseEntity<Map<String, Object>> checkInfo(@RequestBody SerialRequestDto serial) {
-        try {
-            iotService.checkInfo(serial.getSerial());
-            return handleSuccess();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return handleError();
-        }
+  @PostMapping("/fire")
+  public ResponseEntity<Map<String, Object>> sendFireAlert(@RequestBody SerialRequestDto serial) {
+    try {
+      iotService.sendFireSms(serial.getSerial());
+      return handleSuccess();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return handleError();
     }
+  }
 
-    @PostMapping("/check-device")
-    public ResponseEntity<?> checkDevice(@RequestBody SerialRequestDto serial) {
-        try {
-            Map<String, Boolean> result = new HashMap<>();
-            if (iotService.activeCheck(serial.getSerial())) {
-                result.put("usable", true);
-            } else {
-                result.put("usable", false);
-            }
-            return new ResponseEntity<>(result, HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return handleError();
-        }
+  @PostMapping("/check-info")
+  public ResponseEntity<Map<String, Object>> checkInfo(@RequestBody SerialRequestDto serial) {
+    try {
+      iotService.checkInfo(serial.getSerial());
+      return handleSuccess();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return handleError();
     }
+  }
+
+  @PostMapping("/check-device")
+  public ResponseEntity<?> checkDevice(@RequestBody SerialRequestDto serial) {
+    try {
+      Map<String, Boolean> result = new HashMap<>();
+      if (iotService.activeCheck(serial.getSerial())) {
+        result.put("usable", true);
+      } else {
+        result.put("usable", false);
+      }
+      return new ResponseEntity<>(result, HttpStatus.OK);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return handleError();
+    }
+  }
 
   @GetMapping("/check-status")
   public ResponseEntity<?> checkStatus() {
@@ -74,59 +85,59 @@ public class IotController {
     }
   }
 
-    /**
-     * 작성자 : 이성목
-     * <p>
-     * 기기 등록하고 활성화 상태로 변경
-     *
-     * @param code
-     * @return
-     */
-    @PostMapping
-    public ResponseEntity postIotDevice(@RequestBody String code) {
-        String userId = JwtUtil.getMemberId().get();
-        try {
-            iotService.registSerialCode(code, userId);
-            return handleSuccess();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return handleError();
-        }
+  /**
+   * 작성자 : 이성목
+   * <p>
+   * 기기 등록하고 활성화 상태로 변경
+   *
+   * @param code
+   * @return
+   */
+  @PostMapping
+  public ResponseEntity postIotDevice(@RequestBody String code) {
+    String userId = JwtUtil.getMemberId().get();
+    try {
+      iotService.registSerialCode(code, userId);
+      return handleSuccess();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return handleError();
     }
+  }
 
-    /**
-     * 작성자 : 이성목
-     * <p>
-     * 시리얼 코드 변경
-     *
-     * @param code
-     * @return
-     */
-    @PutMapping
-    public ResponseEntity putIotDevice(@RequestBody String code) {
-        // TODO : 이음 코드 수정
+  /**
+   * 작성자 : 이성목
+   * <p>
+   * 시리얼 코드 변경
+   *
+   * @param code
+   * @return
+   */
+  @PutMapping
+  public ResponseEntity putIotDevice(@RequestBody String code) {
+    // TODO : 이음 코드 수정
 
-        String userId = JwtUtil.getMemberId().get();
+    String userId = JwtUtil.getMemberId().get();
 
-        try {
-            iotService.updateSerialCode(code, userId);
-            return handleSuccess();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return handleError();
-        }
+    try {
+      iotService.updateSerialCode(code, userId);
+      return handleSuccess();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return handleError();
     }
+  }
 
-    private ResponseEntity<Map<String, Object>> handleSuccess() {
-        Map<String, Object> result = new HashMap<>();
-        result.put("success", true);
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
+  private ResponseEntity<Map<String, Object>> handleSuccess() {
+    Map<String, Object> result = new HashMap<>();
+    result.put("success", true);
+    return new ResponseEntity<>(result, HttpStatus.OK);
+  }
 
-    private ResponseEntity<Map<String, Object>> handleError() {
-        Map<String, Object> result = new HashMap<>();
-        result.put("success", false);
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
+  private ResponseEntity<Map<String, Object>> handleError() {
+    Map<String, Object> result = new HashMap<>();
+    result.put("success", false);
+    return new ResponseEntity<>(result, HttpStatus.OK);
+  }
 
 }
